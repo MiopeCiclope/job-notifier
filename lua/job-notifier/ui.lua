e8321deb10a26a36ceac9db39a56c8539d309bd9
Romulo Tone <romulo.tone@etraveligroup.com>
2024-10-01 19:27:18 +0200

    making terminal view

--- lua/job-notifier/ui.lua
+++ lua/job-notifier/ui.lua
@@ -1,43 +1,53 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local config = require("telescope.config").values
-local actions = require('telescope.actions')
-local previewers = require('telescope.previewers')
+local actions = require("telescope.actions")
+local previewers = require("telescope.previewers")
+local action_state = require("telescope.actions.state")
 
 local M = {}
 
 M.listLogs = function(opts)
   opts = opts or {}
+
   local dataDir = vim.fn.stdpath("data")
   local formatted_time = os.date("%Y%m%d")
 
   local file_list = {
+    dataDir .. "/job-scanner/react/" .. formatted_time .. ".log",
     dataDir .. "/job-scanner/build/" .. formatted_time .. ".log",
     dataDir .. "/job-scanner/watcher/" .. formatted_time .. ".log",
-    dataDir .. "/job-scanner/react/" .. formatted_time .. ".log",
   }
 
-  pickers.new(opts, {
-    prompt_title = "Static File List with Preview",
-    finder = finders.new_table {
-      results = file_list
-    },
-    sorter = config.generic_sorter(opts),
+  pickers
+      .new(opts, {
+        prompt_title = "Log list",
+        finder = finders.new_table({
+          results = file_list,
+        }),
+        sorter = config.generic_sorter(opts),
 
-    -- Previewer to show the contents of the selected file
-    previewer = previewers.cat.new(opts),
-
-    -- Mappings for selecting an entry
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        local selection = actions.get_selected_entry()
-        actions.close(prompt_bufnr)
-        print("You selected: " .. selection.value)
-        -- You can add more logic here, like opening the selected file
-      end)
-      return true
-    end,
-  }):find()
+        previewer = previewers.new_termopen_previewer({
+          get_command = function(entry)
+            return { "sh", "-c", "tail -n 25 " .. entry.value .. " | cat" }
+          end,
+        }),
+        layout_strategy = "vertical",
+        layout_config = {
+          vertical = {
+            preview_height = 0.6,
+          },
+        },
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            local selection = action_state.get_selected_entry()
+            actions.close(prompt_bufnr)
+            vim.api.nvim_command("edit " .. selection.value)
+          end)
+          return true
+        end,
+      })
+      :find()
 end
 
 return M
